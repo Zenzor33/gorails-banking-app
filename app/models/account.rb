56441e11c5a872e9de8585c4bdbd60c79732206f
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-06-13 17:27:44 +0100

    final

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,18 @@
 class Account < ApplicationRecord
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  def withdraw(amount)
+    update!(balance: balance - amount)
+  end
+
+  def deposit(amount)
+    update!(balance: balance + amount)
+  end
+
+  def transfer(recepient, amount)
+    transaction do
+      withdraw(amount)
+      recepient.deposit(amount)
+    end
+
 end
